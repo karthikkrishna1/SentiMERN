8df58c8053eb72010f02db02c8114a61ef490e5e
Karthik Krishna <karthik4kittu@gmail.com>
2024-01-21 13:34:07 -0500

    update for railwAY

--- sentimentModel/api/main.py
+++ sentimentModel/api/main.py
@@ -7,28 +7,26 @@ from model import clean_message
 
 model = tf.keras.models.load_model('sentiment_model.h5')
 tokenizer = pickle.load(open('tokenizer.pkl','rb'))
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'hsu123nxhsuqw'
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.config['SECRET_KEY'] = 'hsu123nxhsuqw'
+@app.route('/predict', methods = ['POST'])
+def predict_sentiment():
+    data = request.get_json()
+    print(data)
+    
+    clean_text = clean_message(data['text'])
+    sequences = tokenizer.texts_to_sequences([clean_text])
+    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100, padding='post')
+    preds = model.predict(padded_sequences)[0]
+    ans = np.argmax(preds)
+    print(ans)
+    if preds[ans] < 0.5:
+        result = "Not so Sure"
+    else:
+        result = "neutral" if ans == 1 else "positive" if  ans == 2 else "negative"
+    return jsonify(result)
 
-    @app.route('/predict', methods = ['POST'])
-    def predict_sentiment():
-        data = request.get_json()
-        print(data)
-      
-        clean_text = clean_message(data['text'])
-        sequences = tokenizer.texts_to_sequences([clean_text])
-        padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100, padding='post')
-        preds = model.predict(padded_sequences)[0]
-        ans = np.argmax(preds)
-        print(ans)
-        if preds[ans] < 0.5:
-            result = "Not so Sure"
-        else:
-            result = "neutral" if ans == 1 else "positive" if  ans == 2 else "negative"
-        return jsonify(result)
-
-
-    return app
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug=True)
